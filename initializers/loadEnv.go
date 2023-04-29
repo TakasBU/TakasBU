34420aec17aa58576a0a4575800b9748f2271ad0
yoitsmeyusuf <muhammetyusufkaydi@gmail.com>
2023-04-30 01:15:57 +0300

    Update 👍

---
+++ initializers/loadEnv.go
@@ -0,0 +1,44 @@
+package initializers
+
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DBHost         string `mapstructure:"MYSQL_HOST"`
+	DBUserName     string `mapstructure:"MYSQL_USER"`
+	DBUserPassword string `mapstructure:"MYSQL_PASSWORD"`
+	DBName         string `mapstructure:"MYSQL_DB"`
+	DBPort         string `mapstructure:"MYSQL_PORT"`
+	ServerPort     string `mapstructure:"PORT"`
+
+	ClientOrigin string `mapstructure:"CLIENT_ORIGIN"`
+
+	TokenSecret    string        `mapstructure:"TOKEN_SECRET"`
+	TokenExpiresIn time.Duration `mapstructure:"TOKEN_EXPIRED_IN"`
+	TokenMaxAge    int           `mapstructure:"TOKEN_MAXAGE"`
+
+	EmailFrom string `mapstructure:"EMAIL_FROM"`
+	SMTPHost  string `mapstructure:"SMTP_HOST"`
+	SMTPPass  string `mapstructure:"SMTP_PASS"`
+	SMTPPort  int    `mapstructure:"SMTP_PORT"`
+	SMTPUser  string `mapstructure:"SMTP_USER"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigType("env")
+	viper.SetConfigName("app")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
